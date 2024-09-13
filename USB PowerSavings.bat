bd5378590e71a5e594ecbc8014fcc0bc8c17af5e
CleyServices <juliandrexler11@yahoo.com>
2024-09-13 23:33:08 +0200

    Add files via upload

---
+++ USB PowerSavings.bat
@@ -0,0 +1,8 @@
+@echo off
+color 09
+powershell.exe -ExecutionPolicy Bypass -Command "$power_device_enable = Get-WmiObject MSPower_DeviceEnable -Namespace root\wmi; $usb_devices = @('Win32_USBController', 'Win32_USBControllerDevice', 'Win32_USBHub'); foreach ($power_device in $power_device_enable) { $instance_name = $power_device.InstanceName.ToUpper(); foreach ($device in $usb_devices) { foreach ($hub in Get-WmiObject $device) { $pnp_id = $hub.PNPDeviceID; if ($instance_name -like '*'+$pnp_id+'*'){ $power_device.enable = $False; $power_device.psbase.put() } } } }"
+timeout 1 >nul
+cls
+@echo Finished
+timeout 2 >nul
+exit
